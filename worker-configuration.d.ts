bc68fbb6fac95e6a4be054d871b996f1242444f6
READTalk <redtalk.app@hotmail.com>
2026-09-09 19:02:32 +0700

    Rename AUTH_DB to AUTH_DO in type definitions

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -3,7 +3,7 @@
 // Runtime types generated with workerd@1.20260826.1 2025-10-08 nodejs_compat
 interface __BaseEnv_Env {
 	AUTH_KV: KVNamespace;
-	AUTH_DB: D1Database;
+	AUTH_DO: D1Database;
 }
 declare namespace Cloudflare {
 	interface GlobalProps {
